cb95604ef0119f85bd8457cce5308d76b900e8ea
GitDVbootcamp <d_vasquez88@hotmail.com>
2019-02-16 04:50:28 -0700

    update

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
+// Dependencies
+// =============================================================
 var express = require("express");
 var path = require("path");
-var bodyParser = require("body-parser");
 
-
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = 3000;
 
-var PORT = process.env.PORT || 8080;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+
+
+
+
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+//require("./routing/apiRoutes.js")(app);
+//require("./routing/htmlRoutes.js")(app);
+
+
+
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+    console.log("App listening on PORT: " + PORT);
+  });
+  
