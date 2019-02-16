a29a1ec4e1731404801e2a95fd43042dbf46b9fa
GitDVbootcamp <d_vasquez88@hotmail.com>
2019-02-16 03:44:27 -0700

    updated apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,39 @@ var path = require("path");
 
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
     //all friend entries
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    
-}
+    app.post('api/friends', function (req, res) {
+        //capture user input object
+        var userInput = req.body;
+        var userResponses = userInput.scores;
+
+        var matchName = '';
+        var matchImage = '';
+        var totalDifference = 10000;
+
+        for (var i = 0; i < friends.length; i++) {
+            //compute differences
+            var diff = 0;
+            for (var j = 0; j < userResponses.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+            }
+
+            if (diff < totalDifference) {
+                totalDifference = diff;
+                matchName = friends[i].name;
+                matchImage = friends[i].photo;
+            }
+        }
+        //add user
+        friends.push(userInput);
+
+        //send appropriate reponse
+        res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+        });
+
+};
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 
 
+
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 require("./routing/apiRoutes.js")(app);
 require("./routing/htmlRoutes.js")(app);
